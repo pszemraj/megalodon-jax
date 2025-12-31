3c5354e102d4ba45657e14180a9aed32ee3669cb
Peter Szemraj <74869040+pszemraj@users.noreply.github.com>
2025-12-31 05:28:05 -0500

    feat: export TimestepNorm and ComplexEMA from layers module

--- src/megalodon_jax/layers/__init__.py
+++ src/megalodon_jax/layers/__init__.py
@@ -1,9 +1,13 @@
 """Megalodon JAX layers."""
 
+from megalodon_jax.layers.complex_ema import ComplexEMA
 from megalodon_jax.layers.norms import RMSNorm
 from megalodon_jax.layers.rotary import RotaryEmbedding
+from megalodon_jax.layers.timestep_norm import TimestepNorm
 
 __all__ = [
+    "ComplexEMA",
     "RMSNorm",
     "RotaryEmbedding",
+    "TimestepNorm",
 ]
