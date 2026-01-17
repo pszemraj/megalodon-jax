85395084849f9793e9bfa3ca03e5ea472378481f
Peter Szemraj <74869040+pszemraj@users.noreply.github.com>
2026-01-16 19:56:10 -0500

    Add license headers to core modules

--- src/megalodon_jax/layers/complex_ema.py
+++ src/megalodon_jax/layers/complex_ema.py
@@ -1,3 +1,16 @@
+# Copyright 2025 Peter Szemraj.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """Complex Exponential Moving Average (EMA) for Megalodon JAX.
 
 ComplexEMA is the core long-range memory mechanism in Megalodon. It uses
